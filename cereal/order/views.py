f2634babd264cc7a452cdbe879df8a39a492c257
jneo8 <jneo8lin@gmail.com>
2019-02-25 01:11:50 +0800

    shipping percentage

--- cereal/order/views.py
+++ cereal/order/views.py
@@ -3,11 +3,25 @@ from .models import Order
 
 # Create your views here.
 
+def gen_zero_shipping_pct_data():
+    zero_shipping_pct = Order.zero_shipping_pct()
+    return [
+        {
+            "name": "zero",
+            "y": zero_shipping_pct[0],
+        },
+        {
+            "name": "not zero",
+            "y": zero_shipping_pct[1],
+        },
+    ]
+
 def index(request, *args, **kwargs):
+
     return render(
         request,
         "order/index.html",
         {
-            "zero_shipping_pct": Order.zero_shipping_pct(),
+            "zero_shipping_pct": gen_zero_shipping_pct_data(),
         }
     )
