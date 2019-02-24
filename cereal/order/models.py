e3b3d9c1ba6d17e58513d13a4837baec196b6e3c
jneo8 <jneo8lin@gmail.com>
2019-02-24 15:43:11 +0800

    order add new model Order & OrderItem

--- cereal/order/models.py
+++ cereal/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    """Order table."""
+    id = models.IntegerField(primary_key=True)
+    customer = models.IntegerField()  # may be fk.
+    shipping = models.IntegerField()
+    created_at = models.DateTimeField()
+
+
+class OrderItem(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=300)
