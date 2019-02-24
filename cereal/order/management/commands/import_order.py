6ff00b9173b3536bda2ea25db30403a1aed31d52
jneo8 <jneo8lin@gmail.com>
2019-02-24 18:00:56 +0800

    import order data

--- cereal/order/management/commands/import_order.py
+++ cereal/order/management/commands/import_order.py
@@ -1,11 +1,33 @@
 import logging
+import traceback
 from django.core.management.base import BaseCommand, CommandError
 
+import pandas as pd
+
+from ...models import Order
+
 logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     help = "import Order csv file."
 
+    def add_arguments(self, parser):
+        parser.add_argument("file_path", nargs="+", type=str)
+
     def handle(self, *args, **options):
-        logger.debug(args)
-        logger.debug(options)
+        for file in options["file_path"]:
+            try:
+                df = pd.read_csv(file)
+                for idx, row in df.iterrows():
+                    created_at = row["created_at"]
+                    order = Order(
+                        id=row["order_id"],
+                        customer=row["customer_id"],
+                        shipping=row["shipping"],
+                        created_at=row["created_at"],
+                    )
+                    order.save()
+                    logger.debug(row)
+            except Exception as e:
+                logger.error(traceback.format_exc())
--- cereal/order/models.py
+++ cereal/order/models.py
@@ -1,10 +1,29 @@
+import logging
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
-
+logger = logging.getLogger(__name__)
 
 class Order(models.Model):
     """Order table."""
+
+    def save(self, *args, **kwargs):
+
+        # handle created_at
+        created_at = self.created_at
+        if "上午" in created_at or "下午" in created_at:
+            if "上午" in created_at:
+                created_at = created_at.replace("上午", "AM")
+            if "下午" in created_at:
+                created_at = created_at.replace("下午", "PM")
+            logger.debug(created_at)
+            created_at = datetime.strptime(created_at, "%Y/%m/%d %p %H:%M:%S")
+
+        self.created_at = created_at
+
+
+        super().save(*args, **kwargs)
+
     id = models.IntegerField(primary_key=True)
     customer = models.IntegerField()  # may be fk.
     shipping = models.IntegerField()
